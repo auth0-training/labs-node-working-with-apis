fd4267c588efd12aae2150b714ba377c0a3b3895
jesposito <jedidiah.esposito@auth0.com>
2021-08-23 22:02:04 +0000

    added correct starting code state for app and api

--- src/web-app/index.js
+++ src/web-app/index.js
@@ -6,7 +6,6 @@ const logger = require("morgan");
 const path = require("path");
 const { createServer } = require("http");
 const { auth, requiresAuth } = require("express-openid-connect");
-const axios = require("axios").default;
 
 const {
   checkUrl,
@@ -47,17 +46,25 @@ app.use(
   })
 );
 
-app.get("/", async (req, res, next) => {
-  try {
-    const summary = await axios.get(`${API_URL}/total`);
-    res.render("home", {
-      user: req.oidc && req.oidc.user,
-      total: summary.data.total,
-      count: summary.data.count,
-    });
-  } catch (err) {
-    next(err);
-  }
+const expenses = [
+  {
+    date: new Date(),
+    description: "Pizza for a Coding Dojo session.",
+    value: 102,
+  },
+  {
+    date: new Date(),
+    description: "Coffee for a Coding Dojo session.",
+    value: 42,
+  },
+];
+
+app.get("/", async (req, res) => {
+  res.render("home", {
+    user: req.oidc && req.oidc.user,
+    total: expenses.reduce((accum, expense) => accum + expense.value, 0),
+    count: expenses.length,
+  });
 });
 
 app.get("/user", requiresAuth(), async (req, res) => {
@@ -70,15 +77,10 @@ app.get("/user", requiresAuth(), async (req, res) => {
 });
 
 app.get("/expenses", requiresAuth(), async (req, res, next) => {
-  try {
-    const expenses = await axios.get(`${API_URL}/reports`);
-    res.render("expenses", {
-      user: req.oidc && req.oidc.user,
-      expenses: expenses.data,
-    });
-  } catch (err) {
-    next(err);
-  }
+  res.render("expenses", {
+    user: req.oidc && req.oidc.user,
+    expenses,
+  });
 });
 
 // catch 404 and forward to error handler
