e8a637afcb401823c659680f0ebf3aebf131c638
Sara Kazemi <sara.kazemi@okta.com>
2024-07-23 18:04:35 -0700

    mapper api

--- src/api/api/index.js
+++ src/api/api/index.js
@@ -1,3 +1,9 @@
+require("dotenv").config();
+const express = require("express");
+const morgan = require('morgan');
+const cors = require("cors");
+const { createServer } = require("http");
+const { auth, requiredScopes } = require("express-oauth2-jwt-bearer");
 const {
   checkUrl,
   APP_URL, // Public URL for this app
@@ -5,29 +11,44 @@ const {
   AUDIENCE, // Auth0 API Audience List
   PORT,
 } = require("./env-config");
-const express = require("express");
-const cors = require("cors");
-const { createServer } = require("http");
-// 👉 Replace this with express-oauth2-jwt-bearer require 👈
 
 const app = express();
+
+app.use(morgan(':method :url :status :response-time ms - :res[content-length]', {
+  stream: logger.stream
+}));
 
 // Used to normalize URL
 app.use(checkUrl());
 
-app.use(cors());
+app.use(
+  cors({
+    origin: "*",
+    methods: "GET",
+    preflightContinue: false,
+    optionsSuccessStatus: 204,
+  })
+);
 
-const expenses = [
+const geocode = [
   {
-    date: new Date(),
-    description: "Pizza for a Coding Dojo session.",
-    value: 102,
-  },
+    lat: 47.6178819,
+    lng: -122.194041
+  }
+];
+
+const reverse_geocode = [
+  { step: 1, action: 'Turn left' },
+  { step: 2, action: 'Turn right' },
+  { step: 3, action: 'Finish' }
+];
+
+const directions = [
   {
-    date: new Date(),
-    description: "Coffee for a Coding Dojo session.",
-    value: 42,
-  },
+    street: '10900 NE 8th Street',
+    city: 'Bellevue',
+    state: 'Washington'
+  }
 ];
 
 /****************************
@@ -42,17 +63,36 @@ app.get("/", (req, res) => {
 });
 /****************************/
 
-app.get("/total", (req, res) => {
-  const total = expenses.reduce((accum, expense) => accum + expense.value, 0);
-  res.send({ total, count: expenses.length });
+app.get("/api", (req, res) => {
+  if (req.user) {
+    logger.debug('Current user: ' + req.user.sub + ' (scope=' + (req.user.scope || 'N/A') + ')');
+  }
 });
 
-// 👉 Replace this with auth middleware 👈
+// 👆 public routes above 👆
+app.use(auth());
+// 👇 private routes below 👇
 
-app.get("/reports", (req, res) => {
-  res.send(expenses);
+app.get("/api/location/geocode", requiredScopes("read:geocode"), (req, res) => {
+  res.send(geocode);
+});
+
+app.get("/api/location/reverse-geocode", requiredScopes("read:reverse-geocode"), (req, res) => {
+  res.send(reverse_geocode);
+});
+
+app.get("/api/location/directions", requiredScopes("read:directions"), (req, res) => {
+  res.send(directions);
+});
+
+app.use((err, req, res, next) => {
+  res.status(err.status || 500);
+  res.json({
+    status: err.status,
+    message: err.message,
+  });
 });
 
 createServer(app).listen(PORT, () => {
-  console.log(`API: ${APP_URL}`);
+  logger.info(`Worldmappers API (Resource Server) listening on: ${APP_URL}`);
 });
