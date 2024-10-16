94793dbb3f5222bb816d87f133fb94160e1a128c
Sara Kazemi <sara.kazemi@okta.com>
2024-10-16 15:23:05 -0700

    Update next.config.mjs

--- next.config.mjs
+++ next.config.mjs
@@ -1,5 +1,16 @@
 /** @type {import('next').NextConfig} */
+const PORT = "3000";
+let appURL = "localhost:PORT"; // localhost
+// Use the Codespace App URL if we're in the Codespace environment
+if (process.env.CODESPACE_NAME) {
+  appURL = `https://${process.env.CODESPACE_NAME}-${PORT}.${process.env.GITHUB_CODESPACES_PORT_FORWARDING_DOMAIN}`;
+}
 const nextConfig = {
+  experimental: {
+    serverActions: {
+      allowedOrigins: [appURL],
+    },
+  },
   eslint: {
     // ignoreDuringBuilds: true,
   },
